78e8dcb068a8e31b3fb94b349062529a1a6121dd
Kari Matthews <karimatthews3@gmail.com>
2017-07-24 16:31:50 +1000

    change type to work_type

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -71,6 +71,6 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :company, :suburb, :salary, :description, :type, :source_link)
+      params.require(:job).permit(:title, :company, :suburb, :salary, :description, :work_type, :source_link)
     end
 end
