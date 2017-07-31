a6bf032f1f273feb15e68bd486ee6c4e41182aab
Kari Matthews <karimatthews3@gmail.com>
2017-07-31 10:34:30 +1000

    add created_at

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,6 +1,6 @@
 class JobSerializer < ActiveModel::Serializer
   include ActionView::Helpers::NumberHelper
-  attributes :id, :title, :company, :formatted_salary, :description, :suburb, :source_link, :work_type
+  attributes :id, :title, :company, :formatted_salary, :description, :suburb, :source_link, :work_type, :created_at
 
   def formatted_salary
     number_with_delimiter(object.salary)
